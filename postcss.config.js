9c7706549309396ab2eec50e32cc5881c47d6683
Van Jasper <balandoy18@gmail.com>
2026-06-28 12:43:52 +0800

    downgrade postcss-nesting for compatibility

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,7 @@
-import tailwindcssNesting from 'tailwindcss/nesting';
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
 export default {
-    plugins: [
-        tailwindcssNesting(),
-        tailwindcss(),
-        autoprefixer(),
-    ],
+    plugins: {
+        'tailwindcss/nesting': {},
+        tailwindcss: {},
+        autoprefixer: {},
+    },
 };
